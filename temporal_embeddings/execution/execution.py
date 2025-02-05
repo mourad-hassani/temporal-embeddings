b4a50dd1425c5b66408b0e4e541fde3fcbedb3d6
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-02-05 07:47:49 +0100

    Implement DataParallel

--- temporal_embeddings/execution/execution.py
+++ temporal_embeddings/execution/execution.py
@@ -14,7 +14,10 @@ from temporal_embeddings.utils.similarity import asymmetrical_kl_sim
 
 class Execution():
     def __init__(self):
-        self.model: GaussModel = GaussModel(MODEL_NAME, True).eval().to(device=DEVICE)
+        gauss_model: GaussModel = GaussModel(MODEL_NAME, True).eval()
+        gauss_model = torch.nn.DataParallel(gauss_model).to(DEVICE)
+        
+        self.model: GaussModel = gauss_model
         self.tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, model_max_length = MAX_SEQ_LEN, use_fast = False)
 
         self.gauss_data: GaussData = GaussData(INPUT_FILE_PATH, self.tokenizer)
