08ad274da57b9bf7c83fb2f1e179851300f2dd8c
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-02-12 12:31:02 +0100

    Quick fix

--- temporal_embeddings/evaluation/utils/evaluation/temporal_bert_full/temporal_bert_full.py
+++ temporal_embeddings/evaluation/utils/evaluation/temporal_bert_full/temporal_bert_full.py
@@ -93,7 +93,6 @@ def evaluate_model(model_name: str) -> None:
             for paragraph in paragraphs:
                 paragraph_emb = model.encode(paragraph, convert_to_tensor=True)
 
-                print(float(util.cos_sim(question_emb, paragraph_emb)[0].item()))
                 similarities.append(float(util.cos_sim(question_emb, paragraph_emb)[0].item()))
 
             output_similarities.append(similarities)
