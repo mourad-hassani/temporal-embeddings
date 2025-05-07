4854956372bdaa8d6401e33addd800b8248df155
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-05-07 12:30:27 +0200

    Remove loop from sync_local_logs.sh

--- sync_local_logs.sh
+++ sync_local_logs.sh
@@ -1,4 +1,1 @@
-while true; do
-    rsync -avz hassani@slurm:/mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings/logs/ ./logs
-    sleep 20
-done
+rsync -avz hassani@slurm:/mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings/logs/ ./logs
