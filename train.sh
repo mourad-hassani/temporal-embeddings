4a31ece3805390abc8e7a72542bc12a166903f76
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-05-08 12:17:31 +0200

    Add command line arguments to train.sh file

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --time=23:59:00
 #SBATCH --gres=gpu:2
-#SBATCH --nodelist=n102
+#SBATCH --nodelist=${1:-n102}  # Default to n102 if no argument is provided
 
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
-conda activate train-env
-python3 train.py
+conda activate ${2:-train-env}
+python3 ${3:-train.py}
 conda deactivate
