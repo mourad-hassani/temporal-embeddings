1aa7aaea72e68d66c0a1ec951d294f2d5dfdfedf
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-05-09 12:22:02 +0200

    Quick fix

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=23:59:00
 #SBATCH --gres=gpu:2
-#SBATCH --nodelist=n102
+#SBATCH --nodelist=n101
 
 source ~/.bashrc
 cd /mnt/beegfs/home/hassani/training_an_em/project/temporal-embeddings
