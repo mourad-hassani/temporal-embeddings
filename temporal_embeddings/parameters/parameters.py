eb5a951f4e1921857ddf5ea2f6bbf85feb5da71e
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-05-09 12:20:07 +0200

    Reduce the number of evaluations when training

--- temporal_embeddings/parameters/parameters.py
+++ temporal_embeddings/parameters/parameters.py
@@ -17,7 +17,7 @@ INFERENCE_DEVICE: str = "cuda:0"
 DTYPE: torch.dtype = torch.float16
 SEED: int = 0
 TEMPERATURE: float = 0.05
-NUM_EVAL_STEPS: int = 500
+NUM_EVAL_STEPS: int = 1000
 POSITIONAL_ENCODING_DIM: int = 32
 
 INPUT_FILE_PATH: str = Path("data/dataset/dataset.csv")
