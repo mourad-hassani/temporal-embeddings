cdbefc5319441014184105780543327b4f35f2d4
Mourad Hassani <mouradhassani.9101@gmail.com>
2025-05-12 12:14:40 +0200

    Bug fix

--- evaluate.py
+++ evaluate.py
@@ -3,8 +3,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluate a model")
-    parser.add_argument("model_name", type=str, help="Name of the model to evaluate")
-    parser.add_argument("model_path", type=str, help="Path to the model .pth file")
+    parser.add_argument("--model_name", type=str, help="Name of the model to evaluate")
+    parser.add_argument("--model_path", type=str, help="Path to the model .pth file")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size for evaluation")
     parser.add_argument("--max_seq_len", type=int, default=128, help="Maximum sequence length for evaluation")
     args = parser.parse_args()
